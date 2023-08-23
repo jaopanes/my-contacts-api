af4c51fd0fc46e72b23a0fbd0998af4a1c71848a
João Birelo Neto <njoao6016@gmail.com>
2023-08-23 09:08:37 -0300

    first commit

---
+++ src/app/controllers/CategoryController.js
@@ -0,0 +1,61 @@
+const CategoriesRepository = require('../repositories/CategoriesRepository');
+
+class CategoryController {
+  async index(request, response) {
+    const categories = await CategoriesRepository.findAll();
+    response.json(categories);
+  }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const contact = await CategoriesRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'Category not found' });
+    }
+
+    response.json(contact);
+  }
+
+  async store(request, response) {
+    const { name } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const category = await CategoriesRepository.create({ name });
+
+    response.json(category);
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const { name } = request.body;
+
+    const categoryExists = await CategoriesRepository.findById(id);
+
+    if (!categoryExists) {
+      return response.status(404).json({ error: 'Category not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contact = await CategoriesRepository.update(id, { name });
+
+    response.json(contact);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await CategoriesRepository.delete(id);
+
+    response.sendStatus(204);
+  }
+}
+
+module.exports = new CategoryController();
